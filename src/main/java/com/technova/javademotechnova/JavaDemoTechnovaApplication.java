280281e90cf043d1cf3028df2f155f20b0b5e539
Siddhant Thapa Chhetry <90285183+605797@users.noreply.github.com>
2022-03-09 19:33:13 -0600

    Initial commit

---
+++ src/main/java/com/technova/javademotechnova/JavaDemoTechnovaApplication.java
@@ -0,0 +1,13 @@
+package com.technova.javademotechnova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoTechnovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoTechnovaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technova/javademotechnova/JavaDemoTechnovaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technova.javademotechnova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoTechnovaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
